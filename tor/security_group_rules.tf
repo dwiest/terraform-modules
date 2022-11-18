0a6b69d4edc08cba9fa685242d32db02eaf2b184
Damian Wiest <dw9494@gmail.com>
2022-11-18 16:55:01 -0500

    Additional egress rules

--- tor/security_group_rules.tf
+++ tor/security_group_rules.tf
@@ -16,6 +16,16 @@ resource "aws_security_group_rule" "ingress_tor" {
 ##
 ##
 
+resource "aws_security_group_rule" "egress_tor_8080" {
+  description       = "Tor"
+  type              = "egress"
+  from_port         = 8080
+  to_port           = 8080
+  protocol          = "tcp"
+  security_group_id = module.ec2_service.ec2_security_group_id
+  cidr_blocks       = ["0.0.0.0/0"]
+}
+
 resource "aws_security_group_rule" "egress_tor_8888" {
   description       = "Tor"
   type              = "egress"
@@ -40,6 +50,16 @@ resource "aws_security_group_rule" "egress_9002" {
   type              = "egress"
   from_port         = 9002
   to_port           = 9002
+  protocol          = "tcp"
+  security_group_id = module.ec2_service.ec2_security_group_id
+  cidr_blocks       = ["0.0.0.0/0"]
+}
+
+resource "aws_security_group_rule" "egress_9004" {
+  description       = "Tor"
+  type              = "egress"
+  from_port         = 9004
+  to_port           = 9004
   protocol          = "tcp"
   security_group_id = module.ec2_service.ec2_security_group_id
   cidr_blocks       = ["0.0.0.0/0"]
