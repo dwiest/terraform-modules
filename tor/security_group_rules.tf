84af6664c27a451b42724be7320fe1f53af88636
Damian Wiest <dw9494@gmail.com>
2022-11-18 16:34:35 -0500

    Tor resources

---
+++ tor/security_group_rules.tf
@@ -0,0 +1,46 @@
+##
+##
+##
+
+resource "aws_security_group_rule" "ingress_tor" {
+  description       = "Tor"
+  type              = "ingress"
+  from_port         = 9001
+  to_port           = 9001
+  protocol          = "tcp"
+  security_group_id = module.ec2_service.ec2_security_group_id
+  cidr_blocks       = ["0.0.0.0/0"]
+}
+
+##
+##
+##
+
+resource "aws_security_group_rule" "egress_tor_8888" {
+  description       = "Tor"
+  type              = "egress"
+  from_port         = 8888
+  to_port           = 8888
+  protocol          = "tcp"
+  security_group_id = module.ec2_service.ec2_security_group_id
+  cidr_blocks       = ["0.0.0.0/0"]
+}
+resource "aws_security_group_rule" "egress_tor_9001" {
+  description       = "Tor"
+  type              = "egress"
+  from_port         = 9001
+  to_port           = 9001
+  protocol          = "tcp"
+  security_group_id = module.ec2_service.ec2_security_group_id
+  cidr_blocks       = ["0.0.0.0/0"]
+}
+
+resource "aws_security_group_rule" "egress_9002" {
+  description       = "Tor"
+  type              = "egress"
+  from_port         = 9002
+  to_port           = 9002
+  protocol          = "tcp"
+  security_group_id = module.ec2_service.ec2_security_group_id
+  cidr_blocks       = ["0.0.0.0/0"]
+}
---
+++ tor/variables.tf
@@ -0,0 +1,135 @@
+variable "vpc_id" {
+  description = "Deploy resources to this VPC."
+  type        = string
+}
+
+variable "iam_policy_name" {
+  description = "The name of the IAM policy."
+  type        = string
+  default     = "TorPolicy"
+}
+
+variable "service_role_name" {
+  description = "The name of the EC2 instance role."
+  type        = string
+  default     = "Tor"
+}
+
+variable "iam_role_name" {
+  description = "The name of the IAM role."
+  type        = string
+  default     = "TorRole"
+}
+
+variable "aws_region" {
+  description = "Which AWS region to use."
+  type        = string
+  default     = "us-east-1"
+}
+
+variable ec2_launch_template_name {
+  description = "The name of the EC2 launch template."
+  type        = string
+  default     = "Tor"
+}
+
+variable ec2_launch_template_description {
+  description = "The launch template's description."
+  type        = string
+  default     =  "Tor Server"
+}
+
+variable ec2_root_ebs_volume_size {
+  description = "Size of the root EBS volume in GB."
+  type        = number
+  default     = 8
+}
+
+variable ec2_ebs_optimized {
+  description = "Is the EC2 instance EBS optimized?"
+  type        = bool
+  default     = false
+}
+
+variable ec2_image_id {
+  description = "The AMI image id to use."
+  type        = string
+}
+
+variable ec2_instance_type {
+  description = "The EC2 instance type."
+  type        = string
+}
+
+variable ec2_ssh_key_name {
+  description = "The SSH private key to use for the ec2-user account."
+  type        = string
+  default     = ""
+}
+
+variable ec2_instance_tags {
+  description = "Tags to apply to EC2 instances launched using this template."
+  type        = map
+
+  default = {
+    "Name"       : "Tor"
+    "Managed-By" : "terraform"
+  }
+}
+
+variable ec2_security_group_name {
+  description = "The name of the EC2 security group."
+  type        = string
+  default     = "Tor"
+}
+
+variable ec2_security_group_description {
+  description = "The description of the EC2 security group."
+  type        = string
+  default     = "Tor Server"
+}
+
+variable ec2_security_group_tags {
+  description = "Tags for the EC2 security group."
+  type        = map
+  default     = {
+    Managed-By : "terraform"
+    Name       : "Tor"
+  }
+}
+
+variable ec2_security_groups {
+  description = "Additional security groups for the EC2 instances."
+  type        = list
+  default     = []
+}
+
+variable ec2_subnet {
+  description = "Launch EC2 instances in this subnet."
+  type        = string
+  default     = ""
+}
+
+variable ec2_assign_public_ip {
+  description = "Auto-assign a public IP address to the EC2 instances?"
+  type        = bool
+  default     = false
+}
+
+variable client_prefix_list_ids {
+  description = "A list of prefix list ids for Tor clients."
+  type        = list
+  default     = []
+}
+
+variable client_cidr_blocks {
+  description = "Allows clients from these CIDR blocks."
+  type        = list
+  default     = []
+}
+
+variable client_security_groups {
+  description = "Allows clients from these security groups."
+  type        = list
+  default     = []
+}
